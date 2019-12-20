80291f6acb75899c6b66d6bd602aa0ce81d39110
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-12-20 14:15:57 +0530

    Add EDA

---
+++ 01/01_Password_Validation_Check/pass_val.py
@@ -0,0 +1,47 @@
+import re
+
+test_cases = ["Password Lenght > 8 ",
+              "Atleast a single alphabet ",
+              "Atleast a Captial Letter ",
+              "Atleast a Numberical Digit ",
+              "Atleast a speacial Character "]
+
+def is_valid(input_text):
+    valid = True
+    if(len(input_txt) < 8):
+        print(test_cases[0],"NO")
+        valid = False
+    else:
+        print(test_cases[0], "YES")
+    if not re.search("[a-z]", input_txt):
+        print(test_cases[1], "NO")
+        valid = False
+    else:
+        print(test_cases[1], "YES")
+    if not re.search("[A-Z]", input_txt):
+        print(test_cases[2], "NO")
+        valid = False
+    else:
+        print(test_cases[2], "YES")
+    if not re.search("[0-9]", input_txt):
+        print(test_cases[3], "NO")
+        valid = False
+    else:
+        print(test_cases[2], "YES")
+    if not re.search("[!@#$%^&*()]", input_txt):
+        print(test_cases[4], "NO")
+        valid = False
+    else:
+        print(test_cases[3],"YES")
+    
+    return valid
+
+if __name__ == "__main__":
+    while 1:
+        input_txt = str(input("Enter Your Password: "))
+        res = is_valid(input_txt)
+        if(res == False):
+            print("Invalid Password")
+        else:
+            print("Valid Password")
+            break
