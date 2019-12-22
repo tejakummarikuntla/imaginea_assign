0e85a474aa117bbf8a1e362a98cddf97aab6da83
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-12-23 01:06:28 +0530

    Add pd.pivot_table, CC

---
+++ 01/03_Encode/encode.py
@@ -0,0 +1,17 @@
+def encode(s):
+
+    n = len(s)
+    i=0
+    while(i<n):
+        count = 1;
+        while (i < n - 1 and s[i] == s[i + 1]):
+            count+=1
+            i+=1
+        print(s[i]+str(count),end="")
+        i+=1
+
+
+
+
+inp=input()
+encode(inp)
