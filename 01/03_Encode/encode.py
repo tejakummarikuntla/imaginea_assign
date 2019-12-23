17323719d6fbde32c7f2e407f94c00302cf63226
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-12-23 12:21:05 +0530

    Mod encode, rm rot

--- 01/03_Encode/encode.py
+++ 01/03_Encode/encode.py
@@ -1,17 +1,15 @@
 def encode(s):
-
     n = len(s)
-    i=0
+    i = 0
     while(i<n):
         count = 1;
-        while (i < n - 1 and s[i] == s[i + 1]):
+        while (i<n-1 and s[i]==s[i + 1]):
             count+=1
             i+=1
-        print(s[i]+str(count),end="")
-        i+=1
+        print(s[i]+str(count), end="")
+        i += 1
 
+if __name__ == '__main__':
+    input_str = str(input())
+    encode(input_str)
 
-
-
-inp=input()
-encode(inp)
