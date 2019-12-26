e68aace755baf41feeec1f8a79d659dd591aea4b
tejakummarikuntla <teja.kummarikuntla@gmail.com>
2019-12-26 10:51:04 +0530

    Add lst_cur, rot_mat

--- 01/04_Matrix_Rotation/rot_mat.py
+++ 01/04_Matrix_Rotation/rot_mat.py
@@ -1,48 +1,34 @@
-def rotateMatrix(mat):
-
+def rot_mat(mat):
     if not len(mat):
         return
 
-
-    top = 0
-    bottom = len(mat)-1
+    tp = 0
+    btm = len(mat)-1
 
     left = 0
     right = len(mat[0])-1
 
-    while left < right and top < bottom:
-
-        # Store the first element of next row,
-        # this element will replace first element of
-        # current row
-        prev = mat[top+1][left]
-
-        # Move elements of top row one step right
+    while left < right and tp < btm:
+        prev = mat[tp+1][left]
         for i in range(left, right+1):
-            curr = mat[top][i]
-            mat[top][i] = prev
+            curr = mat[tp][i]
+            mat[tp][i] = prev
             prev = curr
 
-        top += 1
-
-        # Move elements of rightmost column one step downwards
-        for i in range(top, bottom+1):
+        tp += 1
+        for i in range(tp, btm+1):
             curr = mat[i][right]
             mat[i][right] = prev
             prev = curr
 
         right -= 1
-
-        # Move elements of bottom row one step left
         for i in range(right, left-1, -1):
-            curr = mat[bottom][i]
-            mat[bottom][i] = prev
+            curr = mat[btm][i]
+            mat[btm][i] = prev
             prev = curr
 
-        bottom -= 1
-
-        # Move elements of leftmost column one step upwards
-        for i in range(bottom, top-1, -1):
+        btm -= 1
+        for i in range(btm, tp-1, -1):
             curr = mat[i][left]
             mat[i][left] = prev
             prev = curr
@@ -52,19 +38,23 @@ def rotateMatrix(mat):
     return mat
 
 
-def printMatrix(mat):
+def print_mat(mat):
     for row in mat:
         print(row)
 
-
-matrix =[
+if __name__ == '__main__':
+    matrix =[
             [1,  2,  3,  4 ],
             [5,  6,  7,  8 ],
             [9,  10, 11, 12 ],
             [13, 14, 15, 16 ]
         ]
-
-
-matrix = rotateMatrix(matrix)
-# Print modified matrix
-printMatrix(matrix)
+    #mat2 =[
+    #    [1,2,3,4,5],
+    #    [6,7,8,9],8,
+    #    [10,11,23,9,0],
+    #    [54,87,90,12,98],
+    #    [34,56,78,12,67]
+    #]
+    matrix = rot_mat(matrix)
+    print_mat(matrix)
